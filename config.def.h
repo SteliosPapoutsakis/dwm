443753ed1d2a5e937729d80afe59197244ae68ae
Stelios Papoutsakis <stelios.papoutsakis4@gmail.com>
2022-03-24 13:50:05 -0400

    changed key bindings for show and hide to have shiftmask, it was overlapping with making the master window wider

--- config.def.h
+++ config.def.h
@@ -96,8 +96,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,   setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY,                       XK_Home,  setgaps,        {.i = 0  } },
-	{ MODKEY,                       XK_s,      show,           {0} },
-	{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY|ShiftMask,                       XK_s,      show,           {0} },
+	{ MODKEY|ShiftMask,                       XK_h,      hide,           {0} },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
