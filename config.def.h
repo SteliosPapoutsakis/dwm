2a629663d824bf4cffd1daf33f51f3563d234ff2
Stelios Papoutsakis <stelios.papoutsakis4@gmail.com>
2022-09-09 17:05:39 -0400

    added dpass script in dwm since it doesn't work with sxhkd

--- config.def.h
+++ config.def.h
@@ -62,12 +62,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-l", "12", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+// command for dpass script, need since it doesn't work with
+// sxhkd
+static const char *dpasscmd[]  = { "/home/stelios/.local/bin/dpass", NULL };
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
+	{ Mod1Mask,						XK_p,	   spawn, 		   {.v = dpasscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
